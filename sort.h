6a2bbb5d082a4cc04b78a10105f5bd7609e136eb
joe <20902932+osekojoe@users.noreply.github.com>
2023-01-19 09:24:02 -0800

    header file

--- sort.h
+++ sort.h
@@ -1,5 +1,5 @@
-#ifndef _SORT_
-#define _SORT_
+#ifndef SORT_H
+#define SORT_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -18,8 +18,11 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/* printing functions */
 void print_array(const int *array, size_t size);
-void print_list(const listint_t *list)
+void print_list(const listint_t *list);
+
+/* sorting algorithms*/
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -34,4 +37,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+
 #endif
